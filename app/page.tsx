906e96e0b56899dfdce80b8d08472ed75ebf6eba
syprogrammer <sonaly647@gmail.com>
2023-09-06 15:28:25 +0530

    completed header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header/>
+    <main className="">
+      <Header />
+      <Board/>
     </main>
   )
 }
